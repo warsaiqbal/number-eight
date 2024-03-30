39a933e64522cdf1f854dd3892071033794de237
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-03-30 06:54:38 -0700

    Add files via upload

---
+++ 6. Number eight/index.js
@@ -0,0 +1,16 @@
+//Addition
+var num1 = 4;
+var num2 = 4;
+console.log(num1 + num2);
+//Subraction
+var num3 = 12;
+var num4 = 4;
+console.log(num3 - num4);
+//Multiplication
+var num5 = 4;
+var num6 = 2;
+console.log(num5 * num6);
+//Division
+var num7 = 16;
+var num8 = 2;
+console.log(num7 / num8);
---
+++ 6. Number eight/index.ts
@@ -0,0 +1,23 @@
+//Addition
+
+let num1 = 4
+let num2 = 4
+console.log(num1 + num2);
+
+//Subraction
+
+let num3 = 12
+let num4 = 4
+console.log(num3 - num4);
+
+//Multiplication
+
+let num5 = 4
+let num6 = 2
+console.log(num5 * num6);
+
+//Division
+
+let num7 = 16
+let num8 = 2
+console.log(num7 / num8);
